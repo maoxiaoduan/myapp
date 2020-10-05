b4637bdae1f1de7490962fef5a6b05d16fca2935
maoxiaoduan <maoxiaoduan006@163.com>
2020-10-05 20:29:19 +0800

    10.5

--- src/views/mine/add.vue
+++ src/views/mine/add.vue
@@ -1,6 +1,24 @@
 <!--  -->
 <template>
-<div class='add'></div>
+  <div class="add">
+    <div class="address">
+      <van-nav-bar
+        title="标题"
+        left-text="返回"
+        left-arrow
+        @click-left="onClickLeft"
+      />
+      <van-address-list
+        v-model="chosenAddressId"
+        :list="list"
+        :disabled-list="disabledList"
+        disabled-text="以下地址超出配送范围"
+        default-tag-text="默认"
+        @add="onAdd"
+        @edit="onEdit"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,40 +26,35 @@
 //例如：import 《组件名称》 from '《组件路径》';
 
 export default {
-//import引入的组件需要注入到对象中才能使用
-components: {},
-data() {
-//这里存放数据
-return {
-
+  //import引入的组件需要注入到对象中才能使用
+  components: {
+    [AddressList.name]: AddressList,
+    [Toast.name]: Toast,
+    [NavBar.name]: NavBar,
+  },
+  data() {
+    //这里存放数据
+    return {};
+  },
+  //监听属性 类似于data概念
+  computed: {},
+  //监控data中的数据变化
+  watch: {},
+  //方法集合
+  methods: {},
+  //生命周期 - 创建完成（可以访问当前this实例）
+  created() {},
+  //生命周期 - 挂载完成（可以访问DOM元素）
+  mounted() {},
+  beforeCreate() {}, //生命周期 - 创建之前
+  beforeMount() {}, //生命周期 - 挂载之前
+  beforeUpdate() {}, //生命周期 - 更新之前
+  updated() {}, //生命周期 - 更新之后
+  beforeDestroy() {}, //生命周期 - 销毁之前
+  destroyed() {}, //生命周期 - 销毁完成
+  activated() {}, //如果页面有keep-alive缓存功能，这个函数会触发
 };
-},
-//监听属性 类似于data概念
-computed: {},
-//监控data中的数据变化
-watch: {},
-//方法集合
-methods: {
-
-},
-//生命周期 - 创建完成（可以访问当前this实例）
-created() {
-
-},
-//生命周期 - 挂载完成（可以访问DOM元素）
-mounted() {
-
-},
-beforeCreate() {}, //生命周期 - 创建之前
-beforeMount() {}, //生命周期 - 挂载之前
-beforeUpdate() {}, //生命周期 - 更新之前
-updated() {}, //生命周期 - 更新之后
-beforeDestroy() {}, //生命周期 - 销毁之前
-destroyed() {}, //生命周期 - 销毁完成
-activated() {}, //如果页面有keep-alive缓存功能，这个函数会触发
-}
 </script>
 <style lang='scss' scoped>
 //@import url(); 引入公共css类
-
 </style>
